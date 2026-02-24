4fda07935307f29d21dd141458972d561929c860
Jeremiah Lawrence <lawrencejeremy225@gmail.com>
2026-02-24 12:40:13 +0000

    Added Terraform and Ansible scripts with gitignore

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "ubuntu_2204" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu_2204.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  key_name = "terraform-key-new" 
+  tags = {
+    Name = "Jeremiah-Terraform-Server"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
