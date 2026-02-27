9f0d57ab1a7148a1c24969ec4b241d7209169ca1
Jeremiah-cybersec <lawrencejeremy225@gmail.com>
2026-02-27 12:41:59 +0000

    Fix aws_instance data source (lookup existing instance)

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,43 +17,6 @@ data "aws_ami" "ubuntu_2204" {
   }
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-security-group"
-  description = "Allow SSH and HTTP"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web-security-group"
-  }
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu_2204.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name = "terraform-key-new" 
-  tags = {
-    Name = "Jeremiah-Terraform-Server"
-  }
+data "aws_instance" "web" {
+  instance_id            = "i-0a276baa851360af0"
 }
